48832e6396aa86be178f86472b173b55026ad5ef
Alessio Peternelli <alessio.peternelli@gmail.com>
2020-11-20 00:32:57 +0000

    typo

--- src/bot.py
+++ src/bot.py
@@ -19,7 +19,7 @@ async def on_message(message):
         await message.channel.send(f'hey {message.author.display_name}')
     if message.content == 'ping':
         await message.channel.send('pong')
-    if message.author.display_name == 'SillyVan84'
+    if message.author.display_name == 'SillyVan84':
         await message.channel.send('Grande Ale!')
 
 
